cc6f2c460053e72312e2e016638d9302f5856492
Prateek <83650305+astra-at-top@users.noreply.github.com>
2022-06-07 22:05:25 +0530

    Add files via upload

---
+++ day1/day1.js
@@ -0,0 +1,72 @@
+// Day 1: Exercises
+// Write a single line comment which says, comments can make code readable
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+// Declare four variables without assigning values
+
+// Declare four variables with assigned values
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+
+
+
+
+// 1st comments can make code readable
+
+//Welcome to 30DaysOfJavaScript
+
+/* comments can make code readable,
+ easy to reuse and informative  */
+
+ let st = 'strings'
+ let bol = true 
+ let un = undefined
+ let nu = null
+
+
+ let p = (data) => console.log(typeof data)
+ let dis = (data) => console.log(data)
+ p(st);
+ p(bol)
+ p(un)
+ p(nu)
+
+ let one ;
+ let two ;
+ let three ;
+ let four ;
+
+ let one1 = 1;
+ let two2 = 1;
+ let three3 = 3;
+ let four4 = 4;
+
+let  f_name = 'spark'
+let l_name = 'sparky'
+let statur = 'single not ready to mingle '
+let country = 'Bharat'
+let age = 10000
+
+let fname= 'spark' , lname = 'sparky', status = 'single', country1 = 'india' , ages = 2333;
+
+let myAge = 25, yourAge = 30
+
+dis(` i am ${myAge} years old \n you are ${yourAge} years old `)
+
+/* day 1 completed 
+
+it was about coments , variable and datatypes 
+
+*/ 
